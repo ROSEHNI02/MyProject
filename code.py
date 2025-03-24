ed9c500622d1f4a9573257fb8a6d1efe42823347
ROSEHNI02 <mishelkulathunga@gmail.com>
2025-03-24 12:20:25 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,3 +2,8 @@ def celsius_to_fahrenheit(celsius):
     """Converts Celsius to Fahrenheit."""
     print(f"Executing: Converting {celsius}°C to Fahrenheit...")
     return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    """Converts Fahrenheit to Celsius."""
+    print(f"Executing: Converting {fahrenheit}°F to Celsius...")
+    return (fahrenheit - 32) * 5/9
